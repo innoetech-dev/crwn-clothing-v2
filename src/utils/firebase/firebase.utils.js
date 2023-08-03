cf7f273af912be83d3de024d014d03f11158b8c7
innoetech-dev <91023717+innoetech-dev@users.noreply.github.com>
2023-08-03 02:15:39 -0400

    Sign Up Implemented

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,6 +1,6 @@
 import { initializeApp } from 'firebase/app';
 
-import {getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import {getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword } from 'firebase/auth';
 import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore'
 
 // Your web app's Firebase configuration
@@ -25,9 +25,11 @@ const firebaseConfig = {
   export const auth = getAuth();
   export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
 
+  export const signInWithGoogleRedirect = () => signInWithRedirect(auth, provider);
+
   export const db = getFirestore();
 
-  export const createUserDocumentFromAuth = async (userAuth) => {
+  export const createUserDocumentFromAuth = async (userAuth, additionalInformation) => {
         const userDocRef = doc(db, 'users', userAuth.uid)
 
         console.log(userDocRef)
@@ -42,7 +44,8 @@ const firebaseConfig = {
                     await setDoc(userDocRef, {
                         displayName,
                         email,
-                        createdAt
+                        createdAt,
+                        ...additionalInformation
                     });
             }
             catch (error){
@@ -53,3 +56,8 @@ const firebaseConfig = {
 
         return userDocRef;
   };
+
+  export const createAuthUserWithEmailAndPassword = async (email, password) => {
+        if(!email || !password) return;
+        return await createUserWithEmailAndPassword(auth, email, password)
+  }
