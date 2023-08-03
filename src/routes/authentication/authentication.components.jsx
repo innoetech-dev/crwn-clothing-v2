55bdb278cdf16a4c6e99b993065012e4f108032c
innoetech-dev <91023717+innoetech-dev@users.noreply.github.com>
2023-08-03 03:21:03 -0400

    lesson 104

---
+++ src/routes/authentication/authentication.components.jsx
@@ -0,0 +1,15 @@
+
+import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
+import SignInForm from "../../components/sign-in/sign-in-form.component";
+
+const Authentication = () => {
+
+    return (
+        <div>
+            <h1>Sign In page</h1>
+            <SignInForm />
+            <SignUpForm />
+        </div>
+    )
+}
+export default Authentication;
