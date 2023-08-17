79f402fbb204f24bb8afdc4a67a39a4e3eb7ae96
Rubens Benoit <rubensbenoit@innoetech.com>
2023-08-17 08:23:26 -0400

    safe

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -1,18 +1,36 @@
 import './cart-item.styles.scss';
 
-const CartItem = ({ cartItem }) => {
-  const { name, imageUrl, price, quantity } = cartItem;
+const CartItem = ({cartItem}) => {
+  const { name, imageUrl, price, quantity } = cartItem
   return (
     <div className='cart-item-container'>
-      <img src={imageUrl} alt={`${name}`} />
-      <div className='item-details'>
-        <span className='name'>{name}</span>
-        <span className='price'>
-          {quantity} x ${price}
-        </span>
-      </div>
+        <img src={imageUrl} alt={`${name}`} />
+        <div className='item-details'>
+          <span className='name'>{name}</span>
+          <span className='price'>{quantity} x ${price}</span>
+        </div>
     </div>
-  );
-};
+  )
+
+}
 
 export default CartItem;
+
+// import './cart-item.styles.scss';
+
+// const CartItem = ({ cartItem }) => {
+//   const { name, imageUrl, price, quantity } = cartItem;
+//   return (
+//     <div className='cart-item-container'>
+//       <img src={imageUrl} alt={`${name}`} />
+//       <div className='item-details'>
+//         <span className='name'>{name}</span>
+//         <span className='price'>
+//           {quantity} x ${price}
+//         </span>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default CartItem;
